d74e2f90c983886ab832afc81ec522dc4d7051a3
mobarmeg.9@gmail.com <alzeabiesam@gmail.com>
2024-04-23 22:11:06 +0300

    - Launching the project.

---
+++ lib/core/theming/app_styles.dart
@@ -0,0 +1,95 @@
+import 'package:doctor_app/core/theming/app_colors.dart';
+import 'package:doctor_app/core/theming/app_font_weight.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppStyles {
+  static TextStyle font24BlackBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: AppFontWeight.bold,
+    color: Colors.black,
+  );
+
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: AppFontWeight.bold,
+    color: AppColor.primaryColor,
+  );
+
+  static TextStyle font13BlueSemiBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: AppFontWeight.semiBold,
+    color: AppColor.primaryColor,
+  );
+
+  static TextStyle font13DarkBlueMedium = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: AppFontWeight.medium,
+    color: AppColor.darkBlue,
+  );
+
+  static TextStyle font13DarkBlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: AppFontWeight.regular,
+    color: AppColor.darkBlue,
+  );
+
+  static TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: AppFontWeight.bold,
+    color: AppColor.primaryColor,
+  );
+
+  static TextStyle font16WhiteSemiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: AppFontWeight.semiBold,
+    color: Colors.white,
+  );
+
+  static TextStyle font13GreyRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: AppFontWeight.regular,
+    color: AppColor.grey,
+  );
+
+  static TextStyle font13BlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: AppFontWeight.regular,
+    color: AppColor.primaryColor,
+  );
+
+  static TextStyle font14GrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: AppFontWeight.regular,
+    color: AppColor.grey,
+  );
+
+  static TextStyle font14LightGrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: AppFontWeight.regular,
+    color: AppColor.lightGrey,
+  );
+
+  static TextStyle font14DarkBlueMedium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: AppFontWeight.medium,
+    color: AppColor.darkBlue,
+  );
+  static TextStyle font16WhiteMedium = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: AppFontWeight.medium,
+    color: Colors.white,
+  );
+
+  static TextStyle font14BlueSemiBold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: AppFontWeight.semiBold,
+    color: AppColor.primaryColor,
+  );
+
+  static TextStyle font15DarkBlueMedium = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: AppFontWeight.medium,
+    color: AppColor.darkBlue,
+  );
+}
---
+++ lib/features/login/views/widgets/dont_have_account_text.dart
@@ -0,0 +1,32 @@
+import 'package:doctor_app/core/helpers/extensions.dart';
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/core/theming/app_styles.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class DontHaveAccountText extends StatelessWidget {
+  const DontHaveAccountText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Don\'t have an account?',
+            style: AppStyles.font13DarkBlueRegular,
+          ),
+          TextSpan(
+            text: ' Sign Up',
+            style: AppStyles.font13BlueSemiBold,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                context.pushReplacementNamed(Routes.signUpScreen);
+              },
+          ),
+        ],
+      ),
+    );
+  }
+}
